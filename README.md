30bd0a8250014870bd5dd9fd9a48fa2a87aad027
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2023-02-14 20:21:08 +0400

    Accept `remove_blacklisted` flag

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 Install and keep updated `$HOME` dotfiles on multiple machines
 
 This repo host a copy of my personal `$HOME` dotfiles, and also inclues a POSIX
-shell script to easily install and keep them updated on multiple machines. It also follows a files scheme that's compatible with
+shell script to easily install and keep them updated on multiple machines. It
+also follows a files scheme that's compatible with
 [Github Codespaces](https://docs.github.com/en/codespaces/customizing-your-codespace/personalizing-github-codespaces-for-your-account#dotfiles).
 
 **Warning**: be sure you don't have any secret or credential stored in your
@@ -13,22 +14,25 @@ specially if it will be a public one.
 ## How to install
 
 1. Fork this repo
-2. Update `URL` variable [dotfiles.sh](./dotfiles.sh) file to point to the
-   forked repository. Also update any of the other constants or config variables
-   if needed.
-3. Install `git` command on the target machine (AKA your own laptop)
-4. Ensure target machine has been correctly config to access to the forked
+2. Install `git` command on the target machine (AKA your own laptop)
+3. Ensure target machine has been correctly config to access to the forked
    repository, for example by copying to the `$HOME` folder a valid
    `.git-credentials` file if accesing with HTTP, or a `.ssh` folder with a
    properly configured SSH public/private key pair if accessing with SSH.
-5. Execute the script:
+4. Execute the script:
 
    ```sh
-   curl https://raw.github.com/piranna/dotfiles/blob/main/dotfiles.sh | sh
+   URL_PATH=piranna/dotfiles
+
+   URL_PATH=$URL_PATH \
+     curl https://raw.github.com/${URL_PATH}/blob/main/dotfiles.sh \
+     | URL=<your-dotfiles-git-repo-url> sh
    ```
 
-6. Fix any git conflict that's shown on the command line output on another
+5. Fix any git conflict that's shown on the command line output on another
    terminal until there's no more conflict errors.
+6. Optionally, commit and push any other change you want to keep in your forked
+   repository.
 
 ## How to update
 
@@ -42,13 +46,13 @@ Being powered by `git`, you can make use of all usual git workflows:
 
 ## How it works
 
-1. Get into user `$HOME` directory and checks that's not already a git
+1. Get into user `$HOME` directory and checks that there's not already a git
    repository from a previous install, both from us or any other similar
    dotfiles tool.
 2. Init a git repository in the `$HOME` directory itself, and config the remote
    one.
 3. Get local files that has conflicts with the ones on remote repository, and
-   commit them.
+   commit them, to keep track of their original content.
 4. Get all files from remote repository and ask user to fix any git conflict
    until there's none of them.
 5. Upload merged configs to remote repository to keep them sync'ed.
--- dotfiles.sh
+++ dotfiles.sh
@@ -17,43 +17,66 @@ BLACKLIST="dotfiles.sh LICENSE README.md"
 GIT_PULL="git pull origin $BRANCH --allow-unrelated-histories"
 
 
+install()
+{
+  # Check if $HOME has already defined dotfiles, or could overwrite something
+  [ -e ".git"        ] && { >&2 echo "Already is a git repository"   ; exit 3; }
+  [ -e ".gitignore"  ] && { >&2 echo "Already has a .gitignore file" ; exit 4; }
+  [ -e "dotfiles.sh" ] && { >&2 echo "Already has a dotfiles.sh file"; exit 5; }
+  [ -e "LICENSE"     ] && { >&2 echo "Already has a LICENSE file"    ; exit 6; }
+  [ -e "README.md"   ] && { >&2 echo "Already has a README.md file"  ; exit 7; }
+
+  # Create local repository
+  git init --initial-branch=$BRANCH
+  git remote add origin $URL
+
+  # Get conflicting files
+  stderr=`$GIT_PULL 2>&1`
+  files=`echo "$stderr" | grep '^	'`
+
+  # TODO: diferenciate between errors and no conflicting files
+  [ -z "$files" ] && { >&2 echo $stderr ; exit 8; }
+
+  # Commit conflicting local files
+  git add $files
+  echo Initial commit: `cat /etc/hostname` | git commit -F -
+
+  # Merge the dotfiles repository
+  while ! $GIT_PULL --no-rebase
+  do
+    echo
+    echo "Fix the conflicts in another shell and press enter to continue..."
+    read REPLY  # POSIX shell requires at least one argument, also if not used
+  done
+
+  # Update the dotfiles repository
+  git push origin $BRANCH
+
+  remove_blacklisted
+}
+
+# Remove repo files not-needed in local
+remove_blacklisted()
+{
+  rm -f $BLACKLIST
+  git update-index --skip-worktree $BLACKLIST
+  git gc --aggressive --prune=now
+}
+
+
 # Work on $HOME folder
 cd $HOME
 
-# Check if $HOME has already defined dotfiles, or could overwrite something
-[ -e ".git"        ] && { >&2 echo "Already is a git repository"   ; exit 1; }
-[ -e ".gitignore"  ] && { >&2 echo "Already has a .gitignore file" ; exit 2; }
-[ -e "dotfiles.sh" ] && { >&2 echo "Already has a dotfiles.sh file"; exit 3; }
-[ -e "LICENSE"     ] && { >&2 echo "Already has a LICENSE file"    ; exit 4; }
-[ -e "README.md"   ] && { >&2 echo "Already has a README.md file"  ; exit 5; }
+case $1 in
+	""|install)
+		install
+    ;;
 
-# Create local repository
-git init --initial-branch=$BRANCH
-git remote add origin $URL
+	remove_blacklisted)
+		remove_blacklisted
+		;;
 
-# Get conflicting files
-stderr=`$GIT_PULL 2>&1`
-files=`echo "$stderr" | grep '^	'`
-
-# TODO: diferenciate between errors and no conflicting files
-[ -z "$files" ] && { >&2 echo $stderr ; exit 6; }
-
-# Commit conflicting local files
-git add $files
-echo Initial commit: `cat /etc/hostname` | git commit -F -
-
-# Merge the dotfiles repository
-while ! $GIT_PULL --no-rebase
-do
-  echo
-  echo "Fix the conflicts in another shell and press enter to continue..."
-  read REPLY  # POSIX shell requires at least one argument, also if not used
-done
-
-# Update the dotfiles repository
-git push origin $BRANCH
-
-# Remove repo files not-needed in local
-rm -f $BLACKLIST
-git update-index --skip-worktree $BLACKLIST
-git gc --aggressive --prune=now
+	*)
+		exit 64
+		;;
+esac
